84591051945af836a96fe58a0d075c55f3be8205
suliko <79033456672sd@gmail.com>
2021-09-16 15:01:54 +0300

    added db migrate

---
+++ src/database/config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+    [process.env.NODE_ENV]: {
+        dialect: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+    },
+}
---
+++ src/database/migrations/20210916101321-create-user.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+        primaryKey: true,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
